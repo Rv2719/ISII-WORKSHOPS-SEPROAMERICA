61a9253edf70aedc445d62ea79cbcc2ac1e32c76
Mary Silvestre De La Cruz <msilvest@espol.edu.ec>
2021-10-18 22:04:24 -0500

    Método calcular.

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -27,3 +27,22 @@ public class CarInsurance {
 		return this.married;
 	}
 }
+public int calculatePremium(boolean licencia) { //licencia true, we can sell the insurance else not
+	if(!licencia) {
+		return -1;
+	}
+	int result= 500;
+	if(this.age<25 && this.married==false && this.gen=='M') {
+		result+=1500;
+	}
+	if(this.married==true || this.gen=='F') {
+		result-=200;
+	}
+	if(this.age>=45 && this.age<65) {
+		result-=100;
+	}		
+	if(this.age>80) {
+		return -1;
+	}
+	return result;
+}
