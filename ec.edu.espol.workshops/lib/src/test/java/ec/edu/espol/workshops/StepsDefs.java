c2a4e6bad36d7ec0e2522231ab5113e2f5b7b597
Raul Villao <rjvillao@espol.edu.ec>
2021-11-06 23:13:09 -0500

    Arreglos

--- ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
+++ ec.edu.espol.workshops/lib/src/test/java/ec/edu/espol/workshops/StepsDefs.java
@@ -8,21 +8,36 @@ public class StepsDefs {
  private int actualAnswer;
  private boolean license;
  public CarInsurance carInsurance= new CarInsurance();
- @Given("The age is 21 ")
- public void the_age_is_21() {
-	 carInsurance.setAge(21);
+ 
+ 
+ //escenario 3
+ @Given("the age is {int}")
+ public void the_age_is(int entero) {
+	 carInsurance.setAge(entero);
  }
- @And("The gender is M")
- public void the_geneder_is_M() {
-	 carInsurance.setGender('M');
+ @And("the gender is {String}")
+ public void the_geneder_is(String m) {
+	 carInsurance.setGender(m.charAt(0));
  }
- @And("is his status is not married")
- public void his_status_is_not_married(){
-	 carInsurance.setMarried(false);
+ @And("is his status is {String}")
+ public void is_his_status_is(String casada){
+	 boolean cas;
+	 if(casada.equals("married")) {
+		 cas=true;
+	 }else {
+		 cas=false;
+	 }
+	 carInsurance.setMarried(cas);
  }
- @And("he not have license")
- public void dont_hace_lecense() {
-	 license=false;
+ @And("he {String} license")
+ public void he_license(String cadena) {
+	 boolean lic;
+	 if(cadena.equals("have")) {
+		 lic=true;
+	 }else {
+		 lic=false;
+	 }
+	 license=lic;
  }
  
  @When("calculate the insurance")
@@ -30,9 +45,9 @@ public class StepsDefs {
 	 actualAnswer=carInsurance.calculatePremium(license);
  }
  
- @Then ("show 2000")
- public void show() {
-	 assertEquals(0, actualAnswer);
+ @Then ("show {int}")
+ public void show(int expectedVal) {
+	 assertEquals(expectedVal, actualAnswer);
  }
  
 }
