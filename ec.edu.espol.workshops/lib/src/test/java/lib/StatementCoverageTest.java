7c3854a515b878eecbbbc0b17aded0f3b41bff43
Raul Villao <rjvillao@espol.edu.ec>
2021-12-27 21:44:47 -0500

    Commit codigo de Junit.

--- ec.edu.espol.workshops/lib/src/test/java/lib/StatementCoverageTest.java
+++ ec.edu.espol.workshops/lib/src/test/java/lib/StatementCoverageTest.java
@@ -1,9 +1,55 @@
 package lib;
 
-public class StatementCoverageTest {
+import ec.edu.espol.workshops.CarInsurance;
 
-	public StatementCoverageTest() {
-		// TODO Auto-generated constructor stub
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class StatementCoverageTest {
+	CarInsurance seguro=new CarInsurance();
+	
+	//test to check Test-01
+	@Test
+	public void testCalculatePremiumOne() {		
+		seguro.setAge(50);
+		seguro.setGender('M');
+		seguro.setMarried(true);
+		
+		int valSeguro =seguro.calculatePremium(false);
+		assertEquals(-1, valSeguro, 0.0);
+	}
+	
+	//test to check Test-02
+	@Test
+	public void testCalculatePremiumTwo() {		
+		seguro.setAge(24);
+		seguro.setGender('M');
+		seguro.setMarried(false);
+		
+		int valSeguro =seguro.calculatePremium(true);
+		assertEquals(2000 , valSeguro, 0.0);
+	}
+	
+	//test to check Test-03
+	@Test
+	public void testCalculatePremiumThree() {		
+		seguro.setAge(24);
+		seguro.setGender('M');
+		seguro.setMarried(true);
+		
+		int valSeguro =seguro.calculatePremium(true);
+		assertEquals(300 , valSeguro, 0.0);
+	}
+	
+	//test to check Test-04
+	@Test
+	public void testCalculatePremiumFour() {		
+		seguro.setAge(50);
+		seguro.setGender('M');
+		seguro.setMarried(false);
+		
+		int valSeguro =seguro.calculatePremium(true);
+		assertEquals(400 , valSeguro, 0.0);
 	}
 
 }
